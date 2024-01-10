1f4ca62892675c2eed71ae14c424d914a3cbb46d
unknown <kumari.ranu525@gmail.com>
2024-01-11 00:06:36 +0800

    add another controller and router

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require('express');
+const router = require('./routes');
 const app=express();
 const port=8000;
 
 
 //use express router
 app.use('/',require('./routes'));
+
 
 
 app.listen(port,function(err){
