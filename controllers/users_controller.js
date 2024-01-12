3c376528044218250fd9d44b9a4ceafc55d7744a
unknown <kumari.ranu525@gmail.com>
2024-01-12 11:24:47 +0800

    setting static files and connecting mongodb

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,56 @@
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
-    res.end('<h1>User Profile</h1>');
+    return res.render('user_profile',{
+        title: "Codeial|Profile"
+    });
+}
+
+
+//render sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial|Sign Up"
+    });
+}
+
+
+//render sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial|Sign In"
+    });
+}
+
+
+//get the sign-up data
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('Error in finding user in signing up'); return
+        }
+
+        if(!user){
+           User.create(req.body,function(err,user){
+            if(err){
+                console.log('Error in finding user in signing up'); return
+            }
+
+            return res.redirect('/users/sign-in');
+           })}
+           else{
+              return res.redirect('back');
+           }
+        
+    });
+
+}
+
+//signin the user and create session
+module.exports.createSession=function(req,res){
+    //to do later
 }
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express=require('express');
+const cookieParser=require('cookie-parser');
 const router = require('./routes');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 
 
 //use express router
 app.use('/',require('./routes'));
+
+//set up a view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
