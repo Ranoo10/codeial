37b912dcc5c4a396e4b91d9d18c80f029c0c7c4d
unknown <kumari.ranu525@gmail.com>
2024-01-23 10:39:38 +0800

    updating users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 const Post=require('../models/post');
+const User=require('../models/user');
 
-module.exports.home=function(req,res){
+
+module.exports.home=async function(req,res){
 
     // console.log(req.cookies);
 //     Post.find({})
@@ -17,25 +19,28 @@ module.exports.home=function(req,res){
       //   console.log('Error in fetching posts',err);
       //   return res.redirect('back');
       // });
-      //populate the user of each post 
-      Post.find({})
-      .populate('user')
-      .populate({
-        path:'comments',
-        populate:{
-            path:'user',
-        }
-      })
-      .exec()
-      .then(posts => {
+      //populate the user of each post
+      try{
+        const posts=await   Post.find({})
+        .populate('user')
+        .populate({
+          path:'comments',
+          populate:{
+              path:'user',
+          }
+        })
+        .exec();
+        const users=await User.find({});
+
         console.log(req.cookies);
-        return res.render('home', {
+        return res.render('home',{
             title: "Codeial | Home",
-            posts: posts,
-        });
-    })
-    .catch(err => {
-        console.log('Error in fetching posts', err);
-        return res.redirect('back');
-    });
-}
+                      posts: posts,
+                      all_users: users,
+                  });
+              }
+              catch(err){
+                  console.error('Error in fetching posts', err);
+                  return res.redirect('back');
+              };
+        }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,12 +1,46 @@
 const User=require('../models/user');
 
 
-    module.exports.profile=function(req,res){
-        return res.render('user_profile',{
-            title:"profile",
-        })
-        
-    };
+    module.exports.profile=async function(req,res){
+        try{
+            const userId=req.params.id;
+            const user=await User.findById(req.params.id);
+
+            if(!user){
+                return res.redirect('back');
+            }
+
+            return res.render('user_profile',{
+                title:"User Profile",
+                profile_user: user,
+        });
+    }
+    catch(err){
+        console.log('Error in finding user',err);
+        return res.redirect('back');
+    } 
+};   
+       
+module.exports.update=async function(req,res){
+    try{
+        if(req.user.id===req.params.id){
+            const updateUser=await User.findByIdAndUpdate(req.params.id, req.body);
+              if(!updateUser){
+                return res.redirect('back');
+            }
+            return res.redirect('back');
+    }
+   
+
+    else{
+        return res.status(401).send('Unauthorized');
+    }
+}catch(err){
+    console.error('Error in updating user',err);
+    return res.redirect('back');
+}
+};
+
 
 
 //render sign up page
